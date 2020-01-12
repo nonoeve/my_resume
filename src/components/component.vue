7b6c9888c0524b72acba96005812aa0db63e1c67
nonoeve <1421819343@qq.com>
2020-01-12 18:59:13 +0800

    增加了music

--- src/components/component.vue
+++ src/components/component.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="bg">
+      <img src="../imgs/music.png" alt="music" id="music" @click="start">
+      <audio src="../music/good girl.mp3" id="aud" autoplay></audio>
         <full-page :options="options" ref="page">
             <div class="section">
                 <Page1/>
@@ -39,22 +41,54 @@ export default {
         menu: '#menu',
         anchors: ['page1', 'page2', 'page3', 'page4']
       },
-      flag: false
+      flag: false,
+      flagm: true
     }
   },
   methods: {
     tomy () {
       this.$refs.page.api.moveSectionDown()
+    },
+    start () {
+      var music = document.getElementById('music')
+      var aud = document.getElementById('aud')
+      if (this.flagm) {
+        music.classList.add('turn')
+        aud.play()
+        this.flagm = false
+      } else {
+        music.classList.remove('turn')
+        aud.pause()
+        this.flagm = true
+      }
     }
   }
 }
 </script>
 <style>
 .bg{
-    background-color: #F5F5F5;
+position: relative;
+background-color: #F5F5F5;
+}
+.music{
+position: absolute;
+right: 10%;
+z-index: 3;
+top: 23px;
 }
 .section {
 perspective: 500px;
 position: relative;
+}
+.turn{
+animation: turn 5s linear infinite;
+}
+@keyframes turn{
+form {
+transform: rotate(0deg);
+}
+to {
+transform: rotate(360deg);
+}
 }
 </style>
